44ec86d3c9115cf87c8e102548ef42decc64931b
LSpace <tbao12901@gmail.com>
2024-07-12 02:06:23 +0700

    Update Main.php

--- src/LootSpace369/BroadcastMessage/Main.php
+++ src/LootSpace369/BroadcastMessage/Main.php
@@ -14,7 +14,8 @@ class Main extends PluginBase {
     VapmPMMP::init($this);
     $this->saveDefaultConfig();
     System::setInterval(function() {
-      if ($text = $this->getText() !== null) {
+      $text = $this->getText();
+      if ($text !== null) {
         $this->getServer()->broadcastMessage($text[mt_rand(1,count($text))]);
       }
     },$this->getConfig()->get("delay")*1000);
