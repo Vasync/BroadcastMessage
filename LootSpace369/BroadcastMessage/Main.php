c4b6072a2901c1703c599a26c253f37182b936b3
LSpace <tbao12901@gmail.com>
2024-07-12 01:44:23 +0700

    Update Main.php

--- LootSpace369/BroadcastMessage/Main.php
+++ LootSpace369/BroadcastMessage/Main.php
@@ -1,1 +1,26 @@
+<?php
 
+declare(strict_types = 1);
+
+namespace LootSpace369\BroadcastMessage;
+
+use pocketmine\plugin\PluginBase;
+use vennv\vapm\VapmPMMP;
+use vennv\vapm\System;
+
+class Main extends PluginBase {
+
+  public function onEnablr(): void {
+    VapmPMMP::init($this);
+    $this->saveDefaultConfig();
+    System::setInterval(function() {
+      foreach ($this->getText() as $text) {
+        $this->getServer()->broadcastMessage($text);
+      }
+    },$this->getConfig()->get("delay")*1000;
+  }
+
+  public function getText(): array {
+    return $this->getConfig()->get("text");
+  }
+}
